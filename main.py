af8bbde952810c4d27d9c206a5e27bcca0384e54
kei-kusanagi <kei_kusanagi@outlook.com>
2021-12-23 17:18:58 -0600

    convirtiendolo a text en ves de binary

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ def load_user(user_id):
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), nullable=False, unique=True)
-    password = db.Column(db.String(180), nullable=False)
+    password = db.Column(db.Text, nullable=False)
 
 
 class Post(db.Model):
