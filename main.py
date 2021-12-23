3c87d9a128ed3826636f6d6e623919272f6bc6fc
kei-kusanagi <kei_kusanagi@outlook.com>
2021-12-22 18:23:57 -0600

    se re hace el reuiremients.txt

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
 #  r"sqlite:///C:\Users\folkh\Desktop\python-curse\blog_youtube_flask_2\blog.db"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
-app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
+app.config["SECRET_KEY"] = os.environ.get("SECRET")
 
 bcrypt = Bcrypt(app)
 
